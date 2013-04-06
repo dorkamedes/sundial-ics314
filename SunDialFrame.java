5ade09c82229fb903afa4248857a8476f4d69ec6
briceorbryce <briceorbryce@gmail.com>
2013-04-06 02:40:30 -1000

    Update SunDialFrame.java
    added text comment

--- SunDialFrame.java
+++ SunDialFrame.java
@@ -1,12 +1,11 @@
 
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 
 /**
- *
+ * Well this is a test
  * @author briceorbryce
  */
 public class SunDialFrame extends javax.swing.JFrame {
