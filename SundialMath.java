9c19224b38ac510de33169b2fa9952c9c3dadd30
dorkamedes <dorkamedes@gmail.com>
2013-04-24 19:51:17 -1000

    Update SundialMath.java
    CHANGES:
    - SundialMath.java now creates objects
    - all static methods have been changed to non-static
    - added a new getter: getAngleOfHour()
    - renamed old getAngleOfHour() methods to setAngleOfHour()
    - proper conversion of degrees to radians and back for Math functions
    
    ISSUES:
    - doesn't check for proper degrees input ie you can enter 1000 and still get an answer; mathematically, that is correct, but not appropriate for this project; any thoughts? does that matter?
    
    OTHER:
    - thoughts? suggestions?

--- SundialMath.java
+++ SundialMath.java
@@ -6,31 +6,56 @@ import java.math.*;
 
 public class SundialMath
 {
+	private double angleOfHour;
 	
-	//Converts hour to decimal
-	//from stackoverflow5
-	public static double convertHourToDecimal(String degree)
+	//constructor
+	public SundialMath()
+	{
+		angleOfHour = 0;
+	}
+	
+	//return the angleOfHour
+	public double getAngleOfHour()
+	{
+		return angleOfHour;
+	}
+
+	/**			
+		@params:	double t:	time measured from noon in degrees of arc
+					String phi: latitude of sundial location
+		
+		finds the angle of the hour line for the gnomon
+		formula:	tan(d) = tan(t)*sin(phi)
+	 */
+	public void setAngleOfHour(double t, String phi)
+	{	
+		angleOfHour = Math.toDegrees(Math.atan(Math.tan(Math.toRadians(t))*Math.sin(Math.toRadians(convertHourToDecimal(phi)))));
+	}
+	
+	//same as above, but takes a double for phi instead of a string
+	public void setAngleOfHour(double t, double phi)
+	{	
+		angleOfHour = Math.toDegrees(Math.atan(Math.tan(Math.toRadians(t))*Math.sin(Math.toRadians(phi))));
+	}
+	
+	/**				
+		@params:	String degree: the value to convert
+		@return:	double:			the converted value
+		@usage:	45\"30'23 = 45 degrees, 30 minutes, 23 seconds
+					You need that first backslash to indicate that the
+					double quote does not close the string
+					
+					convertHourToDecimal("45\"30'23)
+		
+		Converts hour to decimal;
+		from stackoverflow
+	 */
+	public double convertHourToDecimal(String degree)
 	{
     	if(!degree.matches("(-)?[0-6][0-9]\"[0-6][0-9]\'[0-6][0-9](.[0-9]{1,5})?"))
   	      throw new IllegalArgumentException();
   	      
   		String[] strArray=degree.split("[\"']");
   		return Double.parseDouble(strArray[0])+Double.parseDouble(strArray[1])/60+Double.parseDouble(strArray[2])/3600;
-	}
-	
-	//finds the angle of the hour line for the gnomon
-	//formula:	tan(d) = tan(t)*sin(phi)
-	//d	= angle of the hour line
-	//t	= time measured from noon in degrees of arc
-	//phi = latitude of sundial location
-	public static double getAngleOfHour(double t, String phi)
-	{	
-		return Math.atan(Math.tan(t)*Math.sin(convertHourToDecimal(phi)));
-	}
-	
-	//same as above, but takes a double for phi instead of a string
-	public static double getAngleOfHour(double t, double phi)
-	{	
-		return Math.atan(Math.tan(t)*Math.sin(phi));
 	}
 }
